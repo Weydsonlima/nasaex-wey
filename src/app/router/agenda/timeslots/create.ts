7ad4bbaef090a33de73d6a67b959afb3978d25dd
João Gabriel <joaogabriel9633@gmail.com>
2026-03-16 16:54:09 -0300

    update route

--- src/app/router/agenda/timeslots/create.ts
+++ src/app/router/agenda/timeslots/create.ts
@@ -1,30 +1,36 @@
 import { requiredAuthMiddleware } from "@/app/middlewares/auth";
 import { base } from "@/app/middlewares/base";
+import { DayOfWeek } from "@/generated/prisma/enums";
+import prisma from "@/lib/prisma";
+import { z } from "zod";
 
-export const createTimeSlots = base.use(requiredAuthMiddleware).route({
+export const createTimeSlots = base
+  .use(requiredAuthMiddleware)
+  .route({
     method: "POST",
     path: "/agenda/timeslots/create",
     summary: "Create time slots",
-})
-    .input(z.object({
-        agendaId: z.string(),
-        dayOfWeek: z.nativeEnum(DayOfWeek),
-        startTime: z.string(),
-        endTime: z.string(),
-    }))
-    .handler(async ({ input, context, errors }) => {
-        try {
-            const timeSlots = await prisma.timeSlots.create({
-                data: {
-                    agendaId: input.agendaId,
-                    dayOfWeek: input.dayOfWeek,
-                    startTime: input.startTime,
-                    endTime: input.endTime,
-                },
-            });
+  })
+  .input(
+    z.object({
+      agendaId: z.string(),
+      dayOfWeek: z.enum(DayOfWeek),
+      startTime: z.string(),
+      endTime: z.string(),
+    }),
+  )
+  .handler(async ({ input, context, errors }) => {
+    try {
+      const timeSlots = await prisma.availabilityTimeSlot.create({
+        data: {
+          availabilityId: input.agendaId,
+          startTime: input.startTime,
+          endTime: input.endTime,
+        },
+      });
 
-            return timeSlots;
-        } catch (error) {
-            throw errors.INTERNAL_SERVER_ERROR;
-        }
-    });
+      return timeSlots;
+    } catch (error) {
+      throw errors.INTERNAL_SERVER_ERROR;
+    }
+  });

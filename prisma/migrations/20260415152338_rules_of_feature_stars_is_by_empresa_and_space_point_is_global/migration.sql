ed8aa7c694a3ffeb5a3730cfac9a39a67a7e27ed
João Gabriel <joaogabriel9633@gmail.com>
2026-04-17 11:44:56 -0300

    update

--- prisma/migrations/20260415152338_rules_of_feature_stars_is_by_empresa_and_space_point_is_global/migration.sql
+++ prisma/migrations/20260415152338_rules_of_feature_stars_is_by_empresa_and_space_point_is_global/migration.sql
@@ -7,37 +7,38 @@
 
 */
 -- DropForeignKey
-ALTER TABLE "space_point_rule" DROP CONSTRAINT "space_point_rule_org_id_fkey";
+ALTER TABLE "space_point_rule" DROP CONSTRAINT IF EXISTS "space_point_rule_org_id_fkey";
 
 -- DropIndex
-DROP INDEX "space_point_rule_org_id_action_key";
+DROP INDEX IF EXISTS "space_point_rule_org_id_action_key";
 
 -- DropIndex
-DROP INDEX "space_point_rule_org_id_idx";
+DROP INDEX IF EXISTS "space_point_rule_org_id_idx";
 
 -- DropIndex
-DROP INDEX "user_space_point_org_id_total_points_idx";
+DROP INDEX IF EXISTS "user_space_point_org_id_total_points_idx";
 
 -- DropIndex
-DROP INDEX "user_space_point_user_id_org_id_key";
+DROP INDEX IF EXISTS "user_space_point_user_id_org_id_key";
 
 -- AlterTable
-ALTER TABLE "space_point_rule" DROP COLUMN "org_id";
+ALTER TABLE "space_point_rule" DROP COLUMN IF EXISTS "org_id";
 
 -- AlterTable
-ALTER TABLE "space_point_transaction" ADD COLUMN     "org_id" TEXT;
+ALTER TABLE "space_point_transaction" ADD COLUMN IF NOT EXISTS "org_id" TEXT;
 
 -- CreateIndex
-CREATE UNIQUE INDEX "space_point_rule_action_key" ON "space_point_rule"("action");
+CREATE UNIQUE INDEX IF NOT EXISTS "space_point_rule_action_key" ON "space_point_rule"("action");
 
 -- CreateIndex
-CREATE INDEX "space_point_transaction_org_id_idx" ON "space_point_transaction"("org_id");
+CREATE INDEX IF NOT EXISTS "space_point_transaction_org_id_idx" ON "space_point_transaction"("org_id");
 
 -- CreateIndex
-CREATE INDEX "space_point_transaction_org_id_created_at_idx" ON "space_point_transaction"("org_id", "created_at");
+CREATE INDEX IF NOT EXISTS "space_point_transaction_org_id_created_at_idx" ON "space_point_transaction"("org_id", "created_at");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "user_space_point_user_id_key" ON "user_space_point"("user_id");
+CREATE UNIQUE INDEX IF NOT EXISTS "user_space_point_user_id_key" ON "user_space_point"("user_id");
 
 -- AddForeignKey
-ALTER TABLE "space_point_transaction" ADD CONSTRAINT "space_point_transaction_org_id_fkey" FOREIGN KEY ("org_id") REFERENCES "organization"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "space_point_transaction" ADD CONSTRAINT "space_point_transaction_org_id_fkey" 
+FOREIGN KEY ("org_id") REFERENCES "organization"("id") ON DELETE SET NULL ON UPDATE CASCADE;
